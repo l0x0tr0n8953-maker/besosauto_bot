ef24ed4c241748985538250c1dcc29a3493fb468
l0x0tr0n8953-maker <l0x0tr0n8953@gmail.com>
2026-09-04 11:51:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ logging.basicConfig(level=logging.INFO)
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-ADMIN_ID = int(os.getenv("ADMIN_ID"))
+ADMIN_ID = int(os.getenv("ADMIN_ID","0"))
 
 bot = Bot(token=BOT_TOKEN)
 
